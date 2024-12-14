e49d1d512a5949a7618c1d88e4cea60e0244937c
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2024-12-14 12:58:15 -0400

    adding js file

---
+++ main.js
@@ -0,0 +1,20 @@
+// function between(a, b) {
+//   const result = []; 
+  
+//   for (let i = a; i <= b; i++) {
+//     result.push(i);
+//   }
+  
+//   return result;
+  
+//   }
+
+
+//Arrow function version
+const between = (a, b) => {
+  const result = [];
+    for (let i = a; i <= b; i++) {
+    result.push(i);
+  } 
+  return result;
+}
